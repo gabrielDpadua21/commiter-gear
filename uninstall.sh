5951f3dc47fb8068ff700543cc51b865b34a53c3
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2022-03-04 01:18:26 -0300

    🐞fix: fix bash uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -2,9 +2,9 @@
 if [ $SHELL == "/usr/bin/zsh" ] 
 then
 	sed -i '/<<<commiter>>>/,+6 d' ~/.zshrc
-elif [ $SHELL == "bash" ] 
+elif [ $SHELL == "bash" ] || [ $SHELL == "/bin/bash" ] 
 then
-	sed -i '/<<<commiter>>>/,+6 d' ~/.zshrc
+	sed -i '/<<<commiter>>>/,+6 d' ~/.bashrc
 else 
     echo "Shell not suported"
     exit 1
